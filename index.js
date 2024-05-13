488db491e168442fc28dedd0c4e3d608cb2c5b42
Lachlan <lachyhughes@bigpond.com>
2024-05-13 11:39:21 +0930

    Added baseline function to generate SVG files. Only added text validation so far to prevent more than 3 characters but not yet for color choices

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
 const inquirer = require(`inquirer`);
 const fs = require(`fs`);
+const fileName = "./examples/logo.svg";
+const selectShape = require(`./lib/shapeSelection`);
 
-inquirer
-    .prompt([
-    {
-        type: 'input',
-        name: 'logoText',
-        message: 'Enter upto 3 characters for a logo:',
-    },
-    {
-        type: 'input',
-        name: 'textColor',
-        message: 'Enter a colour for text (Either colour name or Hexadecimal number):'
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Please select a shape for the logo:',
-        choices: ['Square', 'Circle', 'Triangle']
-    },
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'Enter a colour for the shape (Either colour name or Hexadecimal number):'
-    }
-    ]).then((response) => 
-    console.log(`You have selected the characters: ${response.logoText} which will be in the color of ${response.textColor}. Using the ${response.shape} Shape with the shape color of ${response.shapeColor}`));
+function generateLogo(response) {
+    const logo = selectShape(response);
+    fs.writeFile(fileName, logo, () => console.log(`Generated logo.svg`));
+}
+
+function init() {
+    inquirer
+        .prompt([
+        {
+            type: 'input',
+            name: 'logoText',
+            message: 'Enter upto 3 characters for a logo:',
+            validate: (answer) => {
+            if (answer.length > 3) {
+                return console.log("\nText must not be larger than 3 characters! Please check your input and try again.");
+            }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter a colour for text (Either colour name or Hexadecimal number):'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Please select a shape for the logo:',
+            choices: ['Square', 'Circle', 'Triangle']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter a colour for the shape (Either colour name or Hexadecimal number):'
+        }
+        ]).then((response) => {
+            console.log(`You have selected the characters: ${response.logoText} which will be in the color of ${response.textColor}. Using the ${response.shape} Shape with the shape color of ${response.shapeColor}`);
+            generateLogo(response);
+        })
+};
+
+init();
