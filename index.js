11a7f4617efc2e1fb38de68f2a824e8ed13e9041
Lachlan <lachyhughes@bigpond.com>
2024-05-14 21:24:20 +0930

    Added comments to all code, Added a final testing js file and minor updates to testing files.

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+// Here are the required scripts for functions used in this program.
 const inquirer = require(`inquirer`);
 const fs = require(`fs`);
-const fileName = "./examples/logo.svg";
 const selectShape = require(`./lib/shapeSelection`);
 const validateColor = require(`./lib/colorCheck`);
+// This is so we can have our filename and location in one variable
+const fileName = "./examples/logo.svg";
 
+// This function is run after all the responses have been gathered
 function generateLogo(response) {
+    // It then runs the selectShape function to determine which shape was selected and run the correct render function in turn
     const logo = selectShape(response);
+    // After running the correct render function it will write that data to a file named logo.svg and log a console message
     fs.writeFile(fileName, logo, () => console.log(`Generated logo.svg`));
 }
 
+// This function is to be run when the program starts up to begin prompting the user for the questions.
 function init() {
     inquirer
         .prompt([
         {
+            // Ask the user to input the characters, limiting if it is greater than 3
             type: 'input',
             name: 'logoText',
             message: 'Enter upto 3 characters for a logo:',
@@ -24,6 +31,7 @@ function init() {
             }
         },
         {
+            // Ask the user to enter a color for the logo text, color name or Hex value, with error check if it doesnt match the list in colorIndex.js
             type: 'input',
             name: 'textColor',
             message: 'Enter a colour for text using either a colour name with camel casing for 2 word color names (e.g. aliceBlue) or a Hexadecimal value (e.g. #F0F8FF):',
@@ -32,18 +40,20 @@ function init() {
                     if (isValid) {
                         return true;
                     } else {
-                        console.log("\nColor must be a valid name or hexidecimal input, please try again.");
+                        console.log("\nColor must be a valid name or hexidecimal input, Note that not all colors are available, please try again.");
                         return false;
                     }
             }
         },
         {
+            // Ask the user to select a shape, square, circle or triangle
             type: 'list',
             name: 'shape',
             message: 'Please select a shape for the logo:',
             choices: ['Square', 'Circle', 'Triangle']
         },
         {
+            // Ask the user to enter a color for the shape, color name or Hex value, with error check if it doesnt match the list in colorIndex.js
             type: 'input',
             name: 'shapeColor',
             message: 'Enter a colour for text using either a colour name with camel casing for 2 word color names (e.g. aliceBlue) or a Hexadecimal value (e.g. #F0F8FF):',
@@ -52,14 +62,15 @@ function init() {
                     if (isValid) {
                         return true;
                     } else {
-                        console.log("\nColor must be a valid name or hexidecimal input, please try again.");
+                        console.log("\nColor must be a valid name or hexidecimal input, Note that not all colors are available, please try again.");
                         return false;
                     }
             }
         }
+        // It will then collect the response as an object and use it in the generateLogo function
         ]).then((response) => {
             generateLogo(response);
         })
 };
-
+// This is used to run the init function when the file is run in the terminal
 init();
